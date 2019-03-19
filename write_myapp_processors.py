f0bc872a3ac5c9db63d2ae48ac7cf0eebddb619f
karthikeyan9475 <38800871+karthikeyan9475@users.noreply.github.com>
2019-03-19 18:19:28 +0530

    sync with gist

--- write_myapp_processors.py
+++ write_myapp_processors.py
@@ -1,7 +1,4 @@
-import os
-import sys
-import json
-import pickle
+import os,sys,json,requests,pickle
 
 #main_pg_list = ['0169100e-67fc-125a-40c6-7ec3e9d63d73'] # main id will be initially given here
 # outfile = 'D:\\tmp\\nifi_data\\outfile'
@@ -11,11 +8,8 @@ outfile = sys.argv[2]
 
 loop_pg_list = []
 print(main_pg_list)
-main_curl_str = 'curl -i -X GET http://localhost:8080/nifi-api/process-groups/' + main_pg_list[0] + '/process-groups'
-main_json_str = os.popen(main_curl_str).read().split("\n\n")[1]
-main_json_data = json.loads(main_json_str)
-
-
+main_curl_url = 'http://localhost:8080/nifi-api/process-groups/' + main_pg_list[0] + '/process-groups'
+main_json_data = requests.get(main_curl_url).json()
 
 if len(main_json_data) > 0:
 	for data in main_json_data['processGroups']:
@@ -27,9 +21,8 @@ while len(loop_pg_list) > 0:
 	for loop_pg_id in loop_pg_list:
 		loop_pg_list.remove(loop_pg_id)
 		main_pg_list.append(loop_pg_id)
-		loop_curl_str = 'curl -i -X GET http://localhost:8080/nifi-api/process-groups/' + loop_pg_id + '/process-groups'
-		loop_json_str=os.popen(loop_curl_str).read().split("\n\n")[1]
-		loop_json_data = json.loads(loop_json_str)
+		loop_curl_url = 'http://localhost:8080/nifi-api/process-groups/' + loop_pg_id + '/process-groups'
+		loop_json_data = requests.get(loop_curl_url).json()
 		if len(loop_json_data) > 0:
 			for data in loop_json_data['processGroups']:
 				loop_pg_list.append(data['id'])
@@ -41,9 +34,8 @@ processor_name_list = []
 
 if len(main_pg_list) > 0:
 	for pg_id in main_pg_list:
-		curl_str = 'curl -i -X GET http://localhost:8080/nifi-api/process-groups/' + pg_id + '/processors'
-		json_str=os.popen(curl_str).read().split("\n\n")[1]
-		json_data = json.loads(json_str)
+		curl_url = 'http://localhost:8080/nifi-api/process-groups/' + pg_id + '/processors'		
+		json_data = requests.get(curl_url).json()
 		if len(json_data) > 0:
 			for data in json_data['processors']:
 				processor_id_list.append(data['component']['id'])
